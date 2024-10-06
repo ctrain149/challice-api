740fdc6a14328ac59c5d5aa46858e7d454e27f1c
Cup <CTRAIN149@GMAIL.COM>
2024-10-05 22:47:50 -0400

    Initializing springboot api

---
+++ src/main/java/com/example/springapi/SpringapiApplication.java
@@ -0,0 +1,19 @@
+package com.example.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+}
